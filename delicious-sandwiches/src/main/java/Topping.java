63a946c8b8c81b8497a2847fb10cd7e46540f6b7
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-11-07 20:59:25 -0500

    add variables, constructor and getters/setters to topping class

--- delicious-sandwiches/src/main/java/Topping.java
+++ delicious-sandwiches/src/main/java/Topping.java
@@ -1,11 +1,35 @@
-import java.util.List;
+import com.pluralsight.deli.ToppingType;
+
 
 
 public class Topping {
-    private List<Topping> toppings;
+    private String name; //Name of topping
+    private ToppingType type; // (Regular or Premium)
 
-   // Constructor to initialize toppins
-    public Topping(List<Topping> toppings) {
-        this.toppings = toppings;
+    // Constructor to initialize Topping with the name and type
+    public Topping(String name, ToppingType type) {
+        this.name = name;
+        this.type = type;
+
+    }
+// Getter and Setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ToppingType getType() {
+        return type;
+    }
+
+    public void setType(ToppingType type) {
+        this.type = type;
     }
 }
+
+
