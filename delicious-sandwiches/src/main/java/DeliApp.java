279c2c6ee9496855232546f8967f025062ff22f0
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-11-08 15:20:39 -0500

    added constants for bread type, constructor, getter for name and added borders to toppings display

--- delicious-sandwiches/src/main/java/DeliApp.java
+++ delicious-sandwiches/src/main/java/DeliApp.java
@@ -2,11 +2,24 @@ public class DeliApp {
     public static void main(String[] args) {
 
         //Display premium toppings
-        System.out.println("Premium Toppings:");
+        System.out.println("""
+                ╒══════════════════╕
+                  Premium Toppings
+                ╘══════════════════╛
+                """);
         Topping.displayToppings(Topping.PremiumToppings);
 
         // Display all regular toppings
-        System.out.println("\nRegular Toppings:");
+        System.out.println("""
+              ╒══════════════════╕ 
+                Regular Toppings
+              ╘══════════════════╛
+              """);
         Topping.displayToppings(Topping.RegularToppings);
+
+    //
+
     }
+
+
 }
--- delicious-sandwiches/src/main/java/com/pluralsight/deli/BreadType.java
+++ delicious-sandwiches/src/main/java/com/pluralsight/deli/BreadType.java
@@ -1,5 +1,23 @@
 package com.pluralsight.deli;
 
 public enum BreadType {
-    WHITE, WHEAT, RYE, WRAP,
+
+// Constants for each type of bread with their string descriptiongit add -A
+    WHITE("White Bread"), WHEAT("Wheat Bread"), RYE("Rye Bread"), WRAP("Wrap Bread");
+
+
+    private final String name;
+
+    // constructor to initialize instance variable
+    BreadType(String name) {
+        this.name = name;
+
+
+    }
+        //Getter to get the name of bread type
+    public String getName() {
+        return name;
+    }
+
+
 }
